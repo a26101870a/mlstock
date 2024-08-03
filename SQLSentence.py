d7ac22e24a54f7296615794d60b5394c7abd873d
a26101870a <a26101870a@gmail.com>
2024-08-03 10:37:52 +0800

    init

---
+++ SQLSentence.py
@@ -0,0 +1,77 @@
+from typing import Union, List, Tuple, Optional
+from mysql.connector.abstracts import MySQLConnectionAbstract
+from mysql.connector.pooling import PooledMySQLConnection
+import pandas as pd
+from datetime import datetime, timedelta
+
+def InsertSQL(datatable:str, columns:List[str], data:List[Tuple], connection:Union[PooledMySQLConnection, MySQLConnectionAbstract]):
+       
+    sql_insert = f"""
+    INSERT INTO {datatable} ({", ".join(columns)})
+    VALUES ({", ".join(["%s"] * len(columns))})
+    """
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.executemany(sql_insert, data)
+            connection.commit()
+            print(f"Insert Data To {datatable} Successfully")
+    except Exception as e:
+        print(f"Error: {e}")
+        connection.rollback()
+
+def QuerySQL(datatable:str, connection:Union[PooledMySQLConnection, MySQLConnectionAbstract], columns:Optional[List[str]]=None) -> pd.DataFrame:
+
+    columns = "*" if columns is None else ", ".join(columns)
+
+    sql_query = f"SELECT {columns} FROM {datatable}"
+    
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(sql_query)
+            records = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
+            return pd.DataFrame(records, columns=column_names)
+    except Exception as e:
+        print(f"Error: {e}")
+        return pd.DataFrame()
+
+# 需要原因:更新資料庫需要取得最近的一個日期
+def GetLatestDate(connection:Union[PooledMySQLConnection, MySQLConnectionAbstract]) -> datetime.date:
+    sql_query = "SELECT date FROM stock.stock_price order by date desc limit 1;"
+    
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(sql_query)
+            records = cursor.fetchall()
+            return records[0][0]
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+def GetPriceByTypeId(type_id: int, connection:Union[PooledMySQLConnection, MySQLConnectionAbstract]) -> pd.DataFrame:
+
+    sql_query = f"""
+        SELECT 
+            *
+        FROM
+            stock.stock_price
+        WHERE
+            code IN (SELECT 
+                    code
+                FROM
+                    stock.stock_code_type
+                WHERE
+                    stock_type_id = {type_id});
+    """
+
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(sql_query)
+            records = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
+            return pd.DataFrame(records, columns=column_names)
+    except Exception as e:
+        print(f"Error: {e}")
+        return pd.DataFrame()
+    
---
+++ Utility.py
@@ -0,0 +1,54 @@
+import SQLSentence
+import mysql
+import datetime
+import pandas as pd
+
+# Constants
+TABLE_STOCK_CODE = 'stock.stock_code'
+TABLE_STOCK_PRICE = 'stock.stock_price'
+
+# Functions
+def GetStockCodeInformation(connection):
+    df = SQLSentence.QuerySQL(TABLE_STOCK_CODE, connection)
+    return df['code'].tolist(), df.set_index('code')['name'].to_dict()
+
+def GetAllData(connection):
+    df = SQLSentence.QuerySQL(TABLE_STOCK_PRICE, connection)
+    df = df.drop('id', axis=1)
+    df['date'] = pd.to_datetime(df['date'])
+    df = df[df['volume'] != 0]
+    
+    return df
+
+def GetMySQLLoggingInformation():
+    res={}
+    with open('mysql_logging_information.txt', 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            key, value = line.strip().split(':')
+            res[key.strip()] = value.strip()
+    return res
+
+def connect_to_database():
+    information = GetMySQLLoggingInformation()
+    return mysql.connector.connect(
+        host=information['host'],
+        port=information['port'],
+        user=information['user'],
+        password=information['password'],
+        database="stock"
+    )
+    
+def Init():
+    with connect_to_database() as connection:
+    
+        # 检查是否连接成功
+        if connection.is_connected():
+            print(f"Connected to MySQL database {(datetime.now()).strftime('%Y-%m-%d %H:%M:%S')}")
+
+        stock_code_list, code_name_dict = GetStockCodeInformation(connection)
+        df = GetAllData(connection)
+
+        print(f"Close database {(datetime.now()).strftime('%Y-%m-%d %H:%M:%S')}")
+
+        return stock_code_list, code_name_dict, df
