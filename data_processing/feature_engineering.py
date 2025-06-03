1c5789ff425fd9646b409f021a3e77f0ff4c03c1
a26101870a <a26101870a@gmail.com>
2025-06-03 18:50:58 +0800

    feat(feature_engineering): Add Normalize function

--- data_processing/feature_engineering.py
+++ data_processing/feature_engineering.py
@@ -1,6 +1,21 @@
 from . import Indicators
+import pandas as pd
+
+def Zscore_Normalize(df: pd.DataFrame, list_period: list[int] = [5, 10, 20], list_exclude_columns: list[str] =None):
+    if list_exclude_columns is None:
+        list_exclude_columns = []
+
+    float_cols = df.select_dtypes(include='float').columns
+    target_cols = [col for col in float_cols if col not in list_exclude_columns]
+
+    for column in target_cols:
+        for period in list_period:
+            df = Indicators.GenerateZScore(df, period, column)
+
+    return df
 
 def Feature(df):
     df = Indicators.CalculatePercentChange(df)
     df = Indicators.RSIFlag(df)
+    df = Zscore_Normalize(df)
     return df
